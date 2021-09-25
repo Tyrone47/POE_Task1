3e8862abe751801eccaaa5bd2873f69f9b9713f3
Tyrone47 <69624424+Tyrone47@users.noreply.github.com>
2021-09-25 11:43:00 +0200

    Added the Character Class with its empty methods. Also fixed some classes from before(EmptyTile,Obstacle,Tile)

--- POE_Task1/Character.cs
+++ POE_Task1/Character.cs
@@ -6,13 +6,55 @@ namespace POE_Task1
 {
      abstract class Character : Tile
     {
-        public Character(int x, int y) : base(x, y) { }
+        
 
         protected int HP { get;set; }
         protected int maxHP { get; set; }
         protected int damage { get; set; }
 
+        protected int[,] characterVision;
 
+        public enum MovementEnum
+        {
+            NOMOVEMENT,
+            UP,
+            DOWN,
+            LEFT,
+            RIGHT
+        }
+
+        public Character (int xPos , int yPos) : base(xPos, yPos) { }
+
+        public virtual void Attack(Character target)
+        {
+
+        }
+
+        public bool isDead ()
+        {
+            return false;
+        
+        }
+
+        public virtual bool CheckRange(Character target)
+        {
+
+        }
+
+        private int distanceTo(Character target)
+        {
+
+        }
+        public void Move(MovementEnum move)
+        {
+
+        }
+
+        public abstract MovementEnum ReturnMove(MovementEnum move = 0)
+        {
+
+        }
+        public abstract override string ToString();
 
     }
 }
